8e099637627d4659028b96d98e9a65e1135b09be
johhnny escobar <ejohhnny@gmail.com>
2020-04-04 22:04:39 -0500

    Add solution to problem In Search of a Easy Problem

--- src/main/java/code_forces/InSearchOfAnEasyProblem.java
+++ src/main/java/code_forces/InSearchOfAnEasyProblem.java
@@ -1,4 +1,27 @@
 package code_forces;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class InSearchOfAnEasyProblem {
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String poll = br.readLine().trim();
+        boolean foundHard = false;
+
+        for (int i= 0; i < poll.length() && !foundHard;i++){
+            if (poll.charAt(i) == '1'){
+                foundHard = true;
+            }
+        }
+
+        if (foundHard){
+            System.out.println("HARD");
+        } else {
+            System.out.println("EASY");
+        }
+    }
 }
